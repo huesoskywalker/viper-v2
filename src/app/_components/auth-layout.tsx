dba1de9d3a450a4c627c8ef62c5ef9263fcc33f6
huesoskywalker <agustinbigoni@gmail.com>
2023-12-22 19:37:18 -0300

    fix: pop up window close on sign in

--- src/app/_components/auth-layout.tsx
+++ src/app/_components/auth-layout.tsx
@@ -3,19 +3,35 @@ import { useSession } from 'next-auth/react'
 import { PropsWithChildren, useEffect } from 'react'
 import { GlobalNav } from './nav/global-nav'
 import UnauthenticatedLayout from '../_components/unauthenticated-layout'
-import { usePathname } from 'next/navigation'
+import { redirect, usePathname } from 'next/navigation'
+import { useCreateAccountStore } from '../(auth)/i/flow/signup/_stores/create-account-store'
 
 export const AuthLayout = ({ children }: PropsWithChildren) => {
-   const { status } = useSession()
+   const { data: session, status } = useSession()
    const pathname = usePathname()
+   const { redirectStep } = useCreateAccountStore()
 
    useEffect(() => {
       if (status === 'loading') return
    }, [status])
 
-   if (pathname.endsWith('/logout')) {
+   if (pathname.endsWith('/i/flow/logout')) {
       return <>{children}</>
    }
+
+   if (
+      session?.user.role === 'newViper' &&
+      !pathname.endsWith('/i/flow/signin') &&
+      !pathname.endsWith('/i/flow/single_sign_on')
+   ) {
+      redirect('/i/flow/single_sign_on')
+   }
+
+   if (session?.user.role === 'needUpdate' && !pathname.endsWith('/i/flow/signup')) {
+      redirectStep(1)
+      redirect('/i/flow/signup')
+   }
+
    if (status === 'authenticated')
       return (
          <>
