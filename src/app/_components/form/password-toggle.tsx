6864f6e50e3643a22667857eeeb163e1a6d8f54d
huesoskywalker <agustinbigoni@gmail.com>
2023-12-28 13:26:44 -0300

    feat: password toggle with span description

--- src/app/_components/form/password-toggle.tsx
+++ src/app/_components/form/password-toggle.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 import { EyeIcon, EyeOffIcon } from 'lucide-react'
+import { useState } from 'react'
 
 interface PasswordToggleProps {
    showPassword: 'password' | 'text'
@@ -8,17 +9,44 @@ interface PasswordToggleProps {
 }
 
 export const PasswordToggle: React.FC<PasswordToggleProps> = ({ showPassword, onToggle }) => {
+   const [displaySpan, setDisplaySpan] = useState<boolean>(false)
+
+   const handleMouseEnter = () => {
+      if (displaySpan === false) setDisplaySpan(true)
+   }
+
+   const handleMouseLeave = () => {
+      if (displaySpan === true) setDisplaySpan(false)
+   }
+
    return (
-      <Button
-         className={cn(
-            'absolute bottom-3 right-3 rounded-lg border-none data-[state=checked]:bg-viper-forest-green',
-         )}
-         type={'button'}
-         variant={'link'}
-         size={'fit'}
-         onClick={onToggle}
-      >
-         {showPassword === 'password' ? <EyeIcon color="white" /> : <EyeOffIcon color="white" />}
-      </Button>
+      <>
+         <Button
+            className={cn('absolute bottom-3 right-3 rounded-lg border-none hover:bg-overlay/60 ')}
+            type={'button'}
+            variant={'link'}
+            size={'fit'}
+            onClick={onToggle}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+         >
+            {showPassword === 'password' ? (
+               <EyeIcon color="white" />
+            ) : (
+               <EyeOffIcon color="white" />
+            )}
+         </Button>
+         <span
+            className={cn(
+               'absolute -bottom-4 -right-5 rounded-sm bg-overlay/60 px-1.5 py-1 text-xs text-foreground',
+               {
+                  hidden: !displaySpan,
+                  'animate-delay-visibility': displaySpan,
+               },
+            )}
+         >
+            {showPassword === 'password' ? 'Reveal password' : 'Hide password'}
+         </span>
+      </>
    )
 }
