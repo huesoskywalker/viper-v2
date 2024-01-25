1d3d3ee66d228fbb58f936f79a81c65a02798f09
huesoskywalker <agustinbigoni@gmail.com>
2024-01-25 10:38:51 -0300

    style: observer opacity mobile nav bar

--- src/app/_components/nav/global-nav.tsx
+++ src/app/_components/nav/global-nav.tsx
@@ -17,31 +17,36 @@ export function GlobalNav() {
    const linkClass =
       'block rounded-full p-3 w-fit text-md font-medium text-foreground hover:bg-accent'
 
-   //TODO: need to handle the opacity of the nav when scrolling on mobile and create the upper menu (avatar and settings wheel)
    return (
-      <div className="fixed bottom-0 z-10 flex w-full flex-col justify-between bg-background py-2 sm:top-0 sm:w-24 sm:items-center sm:border-r sm:border-gray-700 xl:w-72 xl:pl-6 ">
-         <nav className="flex w-full flex-col sm:items-center xl:px-8">
-            <MobileNavBar />
-            <div className="mx-8 flex flex-row items-center justify-between py-2 sm:mx-0 sm:w-full sm:flex-col sm:py-0 xl:items-start">
-               <Link href="/" className={cn(linkClass, 'hidden sm:block')}>
-                  <Image
-                     src={'/viper.png'}
-                     width={30}
-                     height={30}
-                     loading="lazy"
-                     alt="V logo"
-                     className="invert-image"
-                  />
-               </Link>
-               {navItems.map((item) => (
-                  <GlobalNavItem key={item.slug} item={item} username={session?.user.username} />
-               ))}
-               <MoreNavMenu />
-            </div>
-         </nav>
-         <Suspense fallback={<ViperNavSkeleton />}>
-            <ViperNavMenu />
-         </Suspense>
+      <div className="relative z-10">
+         <MobileNavBar />
+         <div className="fixed bottom-0 flex w-full flex-col justify-between bg-background py-2 sm:top-0 sm:w-24 sm:items-center sm:border-r sm:border-gray-700 xl:w-72 xl:pl-6 ">
+            <nav className="flex w-full flex-col sm:items-center xl:px-8">
+               <div className="mx-8 flex flex-row items-center justify-between py-2 sm:mx-0 sm:w-full sm:flex-col sm:py-0 xl:items-start">
+                  <Link href="/" className={cn(linkClass, 'hidden sm:block')}>
+                     <Image
+                        src={'/viper.png'}
+                        width={30}
+                        height={30}
+                        loading="lazy"
+                        alt="V logo"
+                        className="invert-image"
+                     />
+                  </Link>
+                  {navItems.map((item) => (
+                     <GlobalNavItem
+                        key={item.slug}
+                        item={item}
+                        username={session?.user.username}
+                     />
+                  ))}
+                  <MoreNavMenu />
+               </div>
+            </nav>
+            <Suspense fallback={<ViperNavSkeleton />}>
+               <ViperNavMenu />
+            </Suspense>
+         </div>
       </div>
    )
 }
--- src/app/_components/nav/mobile-nav-bar.tsx
+++ src/app/_components/nav/mobile-nav-bar.tsx
@@ -1,20 +1,44 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 import { ViperMobileNavMenu } from './viper-mobile-nav-menu'
+import { cn } from '@/lib/utils'
+
+// TODO: does not work in profile (CHECK where else)
+// Also use a children for the for the middle search bar or logo
+// at the right settings
 
 const MobileNavBar = () => {
+   const ref = useRef<HTMLElement>(null)
+   const [isIntersecting, setIntersecting] = useState<boolean>(true)
+
+   useEffect(() => {
+      if (!ref.current) return
+      const observer = new IntersectionObserver(([entry]) => setIntersecting(entry.isIntersecting))
+
+      observer.observe(ref.current)
+      return () => observer.disconnect()
+   }, [])
+
    return (
-      <div className="fixed top-0 flex w-full items-center justify-between bg-background p-4 py-2 sm:hidden sm:h-fit sm:w-fit">
-         <ViperMobileNavMenu />
-         <Image
-            src={'/viper.png'}
-            width={25}
-            height={25}
-            loading="lazy"
-            alt="V logo"
-            className="block invert-image sm:hidden"
-         />
-      </div>
+      <header ref={ref}>
+         <nav
+            className={cn(
+               'fixed inset-x-0 top-0 flex w-full items-center justify-between bg-background p-4 py-2 backdrop-blur duration-200 sm:hidden sm:h-fit sm:w-fit',
+               isIntersecting ? 'bg-background' : 'bg-background/80 ',
+            )}
+         >
+            <ViperMobileNavMenu />
+            <Image
+               src={'/viper.png'}
+               width={25}
+               height={25}
+               loading="lazy"
+               alt="V logo"
+               className="block invert-image sm:hidden"
+            />
+         </nav>
+      </header>
    )
 }
 
